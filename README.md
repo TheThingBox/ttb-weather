8bbaa155066120506ade5ec92d1b8c0fbd080bd2
lucaspouchot <lucas.pouchot@gmail.com>
2019-03-15 11:46:38 +0100

    init

---
+++ README.md
@@ -0,0 +1,98 @@
+[![License](https://img.shields.io/badge/license-WTFPL-blue.svg)](http://www.wtfpl.net/)
+![GitHub issues](https://img.shields.io/github/issues-raw/thethingbox/ttb-waether.svg)
+![GitHub package.json version](https://img.shields.io/github/package-json/v/thethingbox/ttb-waether.svg)
+
+# ttb-waether
+
+A node module to retreive weather from the openweathermap.org APIs for TheThingBox.
+
+# Usages
+
+## Actual weather
+
+#### sample
+```
+const Weather = require('ttb-weather')
+const weather = new Weather()
+
+weather
+  .setAPI({ key: "45bbf485e06dfa30c7d7138030f25ed6"})
+  .setPosition({city:"Argentan", countryCode: 'fr'})
+  .setUnit(Weather.UNIT.CELSIUS)
+  .setMode(Weather.MODE.WEATHER)
+  .setLang(Weather.LANG.FR)
+  .get()
+  .then( data => {
+    console.log(data)
+  })
+```
+
+#### output
+
+``` json
+{
+  "weather": {
+    "forecast": false,
+    "day": false,
+    "date": "2019-03-15T10:30:17.000Z",
+    "sunrise": "2019-03-15T06:14:23.000Z",
+    "sunset": "2019-03-15T18:03:57.000Z",
+    "temperature": 11.56,
+    "humidity": 87,
+    "wind_speed": 42.48,
+    "condition": "cloudy"
+  },
+  "text": {
+    "normal": "il fait fortement nuageux",
+    "simple": "fortement nuageux"
+  },
+  "icon": {
+    "path": "/root/userdir/node_modules/ttb-node-meteo-2/nodes/lib/icons/cloudy.png",
+    "name": "cloudy"
+  }
+}
+```
+
+## Forecast
+```
+const Weather = require('ttb-weather')
+const weather = new Weather()
+
+let date = new Date()
+date.setDate(date.getDate()+3)
+date.setHours(10)
+
+weather.setAPI({ key: "45bbf485e06dfa30c7d7138030f25ed6"})
+  .setPosition({city:"Argentan", countryCode: 'fr'})
+  .setUnit(Weather.UNIT.CELSIUS)
+  .setMode({ type: Weather.MODE.FORECAST, date: date, allDay: false, tz: "+01:00"})
+  .setLang(Weather.LANG.EN)
+  .get()
+  .then( data => {
+    console.log(data)
+  })
+```
+
+#### output
+
+``` json
+{
+  "weather": {
+    "forecast": true,
+    "day": false,
+    "date": "2019-03-18T09:00:00.000Z",
+    "temperature": 7.02,
+    "humidity": 95,
+    "wind_speed": 15.73,
+    "condition": "rain_day"
+  },
+  "text": {
+    "normal": "there are light rain showers",
+    "simple": "light rain showers"
+  },
+  "icon": {
+    "path": "/root/userdir/node_modules/ttb-node-meteo-2/nodes/lib/icons/rain_day.png",
+    "name": "rain_day"
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,437 @@
+const axios = require('axios')
+const moment = require('moment')
+const i18n = require('i18n')
+const path = require('path')
+const fs = require('fs')
+
+var Weather = function(params = {}) {
+  this.data = []
+
+  this.cacheSize = 5
+  this.currentIndex = 0
+
+  this.api = null
+  this.mode = null
+  this.unit = null
+  this.position = null
+  this.lang = null
+  this.i18n = {}
+
+  i18n.configure({
+    locales: [Weather.LANG.EN, Weather.LANG.FR],
+    register: this.i18n,
+    defaultLocale: Weather.LANG.FR,
+    directory: path.join(__dirname, 'locales'),
+    objectNotation: true,
+    api: {
+      '__': 'translate',
+      '__n': 'plural',
+      '__l': 'list',
+      '__mf': 'messageFormat',
+      '__h': 'hash'
+    }
+  });
+  this.i18n.synonym = this.i18n.plural
+
+  this.setAPI({
+    key: null,
+    host: "https://api.openweathermap.org",
+    version: '2.5'
+  })
+  this.setMode(Weather.MODE.ACTUAL)
+  this.setUnit(Weather.UNIT.CELSIUS)
+  this.setPosition({
+    city: "Paris",
+    countryCode: 'fr'
+  })
+  this.setLang(Weather.LANG.FR)
+
+  if(params.apiKey){
+    this.setAPI(params.apiKey)
+  }
+  if(params.mode){
+    this.setMode(params.mode)
+  }
+  if(params.unit){
+    this.setUnit(params.unit)
+  }
+  if(params.position){
+    this.setPosition(params.position)
+  }
+  if(params.lang){
+    this.setLang(params.lang)
+  }
+}
+
+Weather.prototype.setAPI = function(params){
+  if(!Weather.isObject(params)){
+    params = {
+      key: params
+    }
+  }
+  if(!this.api){
+    this.api = {}
+  }
+  if(params.key){
+    this.api.key = params.key
+  }
+  if(params.host){
+    this.api.host = params.host
+  }
+  if(params.version){
+    this.api.version = params.version
+  }
+  return this
+}
+
+Weather.prototype.setMode = function(params = {}){
+  if(!Weather.isObject(params)){
+    params = {
+      type: params
+    }
+  }
+  if(!this.mode){
+    this.mode = {}
+  }
+  if(typeof params.date === 'undefined'){
+    params.date = null
+  }
+  if(typeof params.allDay === 'undefined'){
+    params.allDay = true
+  }
+  if(typeof params.tz === 'undefined'){
+    params.tz = "+00:00"
+  }
+
+  if(Object.keys(Weather.MODE).findIndex(m => Weather.MODE[m] === params.type) !== -1){
+    this.mode = {
+      type: params.type
+    }
+    if(params.type === Weather.MODE.FORECAST){
+      let _date = null
+      if(params.date === null){
+        _date = moment().add(1, 'days').hours(12).minutes(0).seconds(0).milliseconds(0)
+      } else {
+        _date = moment(params.date)
+        if(params.allDay){
+          _date.hours(12)
+        } else {
+          _date.utcOffset(params.tz)
+        }
+        _date.minutes(0).seconds(0).milliseconds(0)
+      }
+
+      _date = _date.toDate()
+
+      this.mode.date = _date,
+      this.mode.allDay = params.allDay,
+      this.mode.tz = params.tz
+    }
+  }
+  return this
+}
+
+Weather.prototype.setUnit = function(unit = ""){
+  if(Object.keys(Weather.UNIT).findIndex(u => Weather.UNIT[u] === unit) !== -1){
+    this.unit = unit
+  }
+  return this
+}
+
+Weather.prototype.setPosition = function(params){
+  if(params.cityID){
+    this.position = {
+      cityID: params.cityID
+    }
+  } else if(params.city && (params.country || params.countryCode)){
+    this.position = {
+      city: params.city,
+      country: params.country,
+      countryCode: params.countryCode
+    }
+  } else if (params.lat && param.lon) {
+    this.position = {
+      lat: params.lat,
+      lon: params.lon
+    }
+  } else if(params.zip && params.countryCode){
+    this.position = {
+      zip: params.zip,
+      countryCode: params.countryCode
+    }
+  }
+  return this
+}
+
+Weather.prototype.setLang = function(lang = ''){
+  if(Object.keys(Weather.LANG).findIndex(l => Weather.LANG[l] === lang) !== -1){
+    this.i18n.setLocale(lang)
+    this.lang = lang
+  }
+  return this
+}
+
+Weather.prototype.getQueryPosition = function(){
+  let res = {}
+  if(this.position.cityID){
+    res.id = this.position.cityID
+  } else if(this.position.city && (this.position.country || this.position.countryCode)){
+    res.q = this.position.city
+    if(this.position.countryCode){
+      res.q = `${res.q},${this.position.countryCode}`
+    } else {
+      res.q = `${res.q},${this.position.country}`
+    }
+  } else if(this.position.lat && this.position.lon){
+    res.lat = this.position.lat
+    res.lon = this.position.lat
+  } else if(this.position.zip && this.position.countryCode){
+    res.zip = `${this.position.zip},${this.position.countryCode}`
+  }
+  return res
+}
+
+Weather.prototype.getRequest = function(){
+  if(!this.api.key){
+    return null
+  }
+  return {
+    url: `${this.api.host}/data/${this.api.version}/${this.mode.type}`,
+    options: {
+      params: Object.assign(
+        {
+          appid: this.api.key,
+          units: this.unit
+        },
+        this.getQueryPosition()
+      )
+    }
+  }
+}
+
+Weather.prototype.allocIndex = function(){
+  let i = 0
+  if(this.currentIndex < this.cacheSize){
+    this.currentIndex = this.currentIndex
+  } else {
+    this.currentIndex = 0
+  }
+  i = this.currentIndex
+  this.data[i] = {}
+  return i
+}
+
+Weather.prototype.fetchRawData = function(){
+  return new Promise( (resolve, reject) => {
+    var _request = this.getRequest()
+    if(_request === null){
+      reject(new Error("The API Key from openweathermap.org must be set. weather.setAPI(apiKey)"))
+      return
+    }
+    axios.get(_request.url, _request.options)
+    .then( resp => {
+      try{
+        resp.data = JSON.parse(resp.data)
+      } catch(e){}
+      if(resp.status !== 0 || !Weather.isObject(resp.data)){
+        var index = this.allocIndex()
+        this.data[index].raw = resp.data
+        this.data[index].date = Date.now()
+        this.data[index].mode = Object.assign({}, this.mode)
+        this.data[index].api = Object.assign({}, this.api)
+        resolve(index)
+      } else {
+        throw new Error(`Unexpected response from ${_request.url}`)
+      }
+    })
+    .catch( err => {
+      reject(err)
+    })
+  })
+}
+
+Weather.prototype.retrieveWeatherFromRawData = function(index = null){
+  return new Promise( (resolve, reject) => {
+    if(index === null){
+      index = this.currentIndex
+    }
+
+    if(this.data[index].api.version === "2.5"){
+      if(this.data[index].mode.type === Weather.MODE.FORECAST && this.data[index].raw.list && this.data[index].raw.list.length > 0){
+        var pdh = this.data[index].mode.date.getHours();
+        pdh = pdh-(pdh%3)
+
+        var pdt = moment(this.data[index].mode.date).hour(pdh).startOf('hour').unix();
+
+        var hdataIndex = this.data[index].raw.list.findIndex(d => d.dt === pdt);
+        if(hdataIndex === -1){
+          hdataIndex = 0
+        }
+
+        this.data[index].weather = {
+          forecast: true,
+          day: this.data[index].mode.allDay === true,
+          date: new Date(this.data[index].raw.list[hdataIndex].dt*1000),
+          temperature: this.data[index].raw.list[hdataIndex].main.temp,
+          humidity: this.data[index].raw.list[hdataIndex].main.humidity,
+          wind_speed: Number((this.data[index].raw.list[hdataIndex].wind.speed*3.6).toFixed(2))
+        }
+
+        this.data[index].weather.condition = Weather.transformOpenWeatherCodeToInternalCode(this.data[index].raw.list[hdataIndex].weather[0].id, this.data[index].raw.list[hdataIndex].weather[0].icon);
+      } else {
+        this.data[index].weather = {
+          forecast: false,
+          day: false,
+          date: new Date(this.data[index].raw.dt*1000),
+          sunrise: new Date(this.data[index].raw.sys.sunrise*1000),
+          sunset: new Date(this.data[index].raw.sys.sunset*1000),
+          temperature: this.data[index].raw.main.temp,
+          humidity: this.data[index].raw.main.humidity,
+          wind_speed: (this.data[index].raw.wind.speed*3.6).toFixed(2)
+        };
+
+        this.data[index].weather.condition = Weather.transformOpenWeatherCodeToInternalCode(this.data[index].raw.weather[0].id, this.data[index].raw.weather[0].icon);
+      }
+      resolve()
+    } else {
+      reject(new Error(`Missing parser fot api version ${this.data[index].api.version}`))
+    }
+  })
+}
+
+Weather.prototype.retrieveTextAndIconFromWeather = function(index = null){
+  if(index === null){
+    index = this.currentIndex
+  }
+  this.data[index].icon = this.weatherConditionToIcon(this.data[index].weather.condition)
+  this.data[index].text = this.weatherConditionToText(this.data[index].weather.condition)
+}
+
+Weather.prototype.weatherConditionToIcon = function(condition){
+  let icon = condition
+  if(icon === 'thunderstorm'){
+    icon = 'stormy'
+  }
+  return {
+    path: path.join(__dirname, 'icons', `${icon}.png`),
+    name: icon
+  }
+}
+
+Weather.prototype.weatherConditionToText = function(condition){
+  var text = ''
+  var text_simple = ''
+  var prefix = ""
+  var indexCondition = 0
+  const _indexCondition = parseInt(this.i18n.translate(`weathers.${condition}.length`), 10)
+  if(!isNaN(_indexCondition)){
+    indexCondition = _indexCondition-1
+  }
+  indexCondition = Weather.randomInt(0, indexCondition)
+
+  var prefix_key = this.i18n.synonym(`weathers.${condition}.prefix_keys`, indexCondition)
+  var prefix = this.weatherGetRandomPrefixByKey(prefix_key)
+
+  text = this.i18n.synonym(`weathers.${condition}.weather`, indexCondition)
+  return {
+    normal: `${prefix} ${text}`.trim(),
+    simple: text
+  }
+}
+
+Weather.prototype.weatherGetRandomPrefixByKey = function(key){
+  var indexPrefix = 0
+  const _indexPrefix = parseInt(this.i18n.translate(`prefixs.${key}.length`), 10)
+  if(!isNaN(_indexPrefix)){
+    indexPrefix = _indexPrefix-1
+  }
+  indexPrefix = Weather.randomInt(0, indexPrefix)
+  return this.i18n.synonym(`prefixs.${key}.prefix`, indexPrefix)
+}
+
+Weather.prototype.get = function(){
+  return new Promise( (resolve, reject) => {
+    let _index = null
+    this.fetchRawData()
+    .then( index => {
+      if(index !== null){
+        _index = index
+        return this.retrieveWeatherFromRawData()
+      } else {
+        throw new Error('Cannot fetch weather')
+      }
+    })
+    .then( () => {
+      this.retrieveTextAndIconFromWeather(_index)
+      resolve({
+        weather: this.data[_index].weather,
+        text: this.data[_index].text,
+        icon: this.data[_index].icon
+      })
+    })
+    .catch(reject)
+  })
+}
+
+Weather.isObject = function(obj){
+  if(obj === null) {
+    return false
+  }
+  return ((typeof obj === 'function') || (typeof obj === 'object'))
+}
+
+Weather.randomInt = function(min = 0, max = 1){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+Weather.transformOpenWeatherCodeToInternalCode = function(x, icon) {
+    var day = icon.endsWith("d");
+    if(x >= 200 && x < 300) return "thunderstorm";
+    else if(x == 800 && day) return "sunny";
+    else if(x == 800) return "night";
+    else if(x == 801 && day) return "cloudy_day";
+    else if(x == 801) return "cloudy_night";
+    else if(x >= 802 && x < 900) return "cloudy";
+    else if(x >= 700 & x < 800) return "fog";
+    else if(x >= 300 & x < 400) return "rain";
+    else if(x == 511) return "meltedsnow";
+
+    if(x >= 500 && x < 600) { // rain
+      if([520, 521, 522, 531].indexOf(x) != -1) return "showerrain";
+      if(day) return "rain_day";
+      return "rain_night";
+    }
+
+    if(x >= 600 && x < 700) { // snow
+      if([611, 612, 615, 616].indexOf(x) != -1) return "meltedsnow";
+      if([602, 621, 622].indexOf(x) != -1) return "snow";
+      if(day) return "snow_day";
+      return "snow_night";
+    }
+}
+
+Weather.MODE = {
+  ACTUAL: 'weather',
+  WEATHER: 'weather',
+  FORECAST: 'forecast',
+  PREVISION: 'forecast'
+}
+
+Weather.UNIT = {
+  C: 'metric',
+  CELSIUS: 'metric',
+  METRIC: 'metric',
+  F: 'imperial',
+  FAHRENHEIT: 'imperial',
+  IMPERIAL: 'imperial'
+}
+
+Weather.LANG = {
+  EN: 'en-US',
+  FR: 'fr-FR',
+  US: 'en-US'
+}
+
+module.exports = Weather
